3872544caa8ce0ce6253762baaae6cdf916ed555
Elizabeth <echurinov@ufl.edu>
2023-01-26 11:22:54 -0500

    Basic key presses for controller

--- makefile
+++ makefile
@@ -3,16 +3,16 @@
 # 	Compiles the C code for the targeted Arduino microcontroller.
 
 # to see the desired port and board-id run: `arduino-cli board list`
-PORT=/dev/cu.usbmodem14101
-BOARD_ID=arduino:avr:pro
+PORT=COM4
+BOARD_ID=arduino:avr:leonardo
 
 .PHONY: cc
 cc:
-	arduino-cli compile --fqbn $(BOARD_ID) . --build-path build --output-dir build/out
+	arduino-cli compile --fqbn arduino:avr:leonardo . --build-path build --output-dir build/out
 
 .PHONY: up
 up:
-	arduino-cli upload --fqbn $(BOARD_ID) -p /dev/cu.usbmodem14101 . --input-dir build/out
+	arduino-cli upload --fqbn arduino-cli compile --fqbn arduino:avr:leonardo -p COM6 --input-dir build/out
 
 .PHONY: clean
 clean:
--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,48 @@ Details:
 
 #include "controller.h"
 #include "arduino.h"
+#include "Keyboard/src/Keyboard.h"
 
-// @note: example code to test using the C++ header/source files
+
+const int UpButton = 5; 
+const int DownButton = 4;
+const int LeftButton = 3;
+const int RightButton = 2;
+
 Controller* c = nullptr; 
 
 void setup() {
     c = new Controller();
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+
+    //Initialize pins
+    pinMode(UpButton, INPUT);
+    pinMode(DownButton, INPUT);
+    pinMode(LeftButton, INPUT);
+    pinMode(RightButton, INPUT);
+    Keyboard.begin();
 }
 
 void loop() {
 
-    uint32_t delta = 1000;
-    if (c->GetId() == 0x03) {
-        delta = 100;
+    if(digitalRead(UpButton) == HIGH){
+        Keyboard.press('w');
+        Keyboard.release('w');
+        //delay(300);
     }
-
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(delta);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(delta);
+   if(digitalRead(DownButton) == HIGH){
+        Keyboard.press('s');
+        Keyboard.release('s');
+	      //delay(300);
+   }
+   if(digitalRead(LeftButton) == HIGH){
+        Keyboard.press('a');
+        Keyboard.release('a');
+	      //delay(300);
+   }
+   if(digitalRead(RightButton) == HIGH){
+        Keyboard.press('d');
+        Keyboard.release('d');
+	      //delay(300);
+   }
 }
