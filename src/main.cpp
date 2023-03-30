5229d94f89fd81de41fe3a1c25a1209e0a98b381
Elizabeth <echurinov@ufl.edu>
2023-03-29 20:25:27 -0400

    Adding library

--- src/main.cpp
+++ src/main.cpp
@@ -11,123 +11,85 @@ Details:
 #include "Joystick/Joystick.h"
 
 
-// WASD CONTROL
-const int W_Button = 5;          //W
-const int S_Button = 4;          //S
-const int A_Button = 3;          //A
-const int D_Button = 2;          //D
+// D-PAD CONTROL
+const int UP_BUTTON = 5;          
+const int DOWN_BUTTON = 4;          
+const int LEFT_BUTTON = 3;          
+const int RIGHT_BUTTON = 2;         
 
 // TO DO: Determine which buttons will control these
 // AB/XY CONTROL
-const int A1_Button = 6;          //
-const int B_Button = 7;          //
-const int X_Button = 8;          //
-const int Y_Button = 9;          //
+const int A_BUTTON = 6;          
+const int B_BUTTON = 7;          
+const int X_BUTTON = 8;          
+const int Y_BUTTON = 9;         
 
 // TO DO: Determine which keyboard buttons will control these
 // HOME/PAUSE BUTTONS
-const int Home_Button = 10;          //
-const int Pause_Button = 16;          //
+const int HOME_BUTTON = 10;         
+const int PAUSE_BUTTON = 16;          
 
-// JOYSTICK 1 CONTROL (MOUSE)
-const int JS_1_Button = 14;  // Push button
+// JOYSTICK 1 CONTROL 
+const int JS_1_BUTTON = 14;  // Push button
 const int JS_1_X = A0;       // X-axis
 const int JS_1_Y = A1;       // Y-axis
 
-const int Range = 12;             // output range of X or Y movement
-const int Delay = 5;              // response delay of the mouse, in ms
-const int Threshold = Range / 4;  // resting threshold
-const int Center = Range / 2;     // resting position value
-
 // JOYSTICK 2 CONTROL
-const int JS_2_Button = 15;  // Push button
+const int JS_2_BUTTON = 15;  // Push button
 const int JS_2_X = A2;       // X-axis
 const int JS_2_Y = A3;       // Y-axis
 
 // GENERAL VARIABLES
-const int DELAY = 190;
+const int DELAY = 10;
 
 Controller* c = nullptr; 
 
-// Function to read X and Y axis for mouse control
-int readAxis(int axis){
-
-  int read = analogRead(axis); //read the current axis
-  read = map(read, 0, 1023, 0, Range);
-  
-  int to_return = read - Center;
-
-  if(abs(to_return) < Threshold){ //don't move mouse if mouse read is too small, prevents joycon drift
-    to_return = 0;
-  }
-  
-  return to_return;
-}
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
+  12, 0,                 // Button Count, Hat Switch Count
+  true, true, true,      // X and Y, but no Z Axis
+  true, true, true,      // Rx, Ry, or Rz
+  false, false,          // No rudder or throttle
+  false, false, false);  // No accelerator, brake, or steering
 
 void setup() {
     c = new Controller();
     Serial.begin(9600);
     
-    //Initialize pins
-    pinMode(W_Button, INPUT);
-    pinMode(S_Button, INPUT);
-    pinMode(A_Button, INPUT);
-    pinMode(D_Button, INPUT);
+    /* Initialize pins */
+    // D-PAD
+    pinMode(UP_BUTTON, INPUT_PULLUP);
+    pinMode(DOWN_BUTTON, INPUT_PULLUP);
+    pinMode(LEFT_BUTTON, INPUT_PULLUP);
+    pinMode(RIGHT_BUTTON, INPUT_PULLUP);
 
-    Keyboard.begin();
-    Mouse.begin();
+    // AB/XY
+    pinMode(A_BUTTON, INPUT_PULLUP);
+    pinMode(B_BUTTON, INPUT_PULLUP);
+    pinMode(X_BUTTON, INPUT_PULLUP);
+    pinMode(Y_BUTTON, INPUT_PULLUP);
+
+    // PAUSE and HOME
+    pinMode(HOME_BUTTON, INPUT_PULLUP);
+    pinMode(PAUSE_BUTTON, INPUT_PULLUP);
+
+    // JOYSTICK 1 control
+    pinMode(JS_1_BUTTON, INPUT_PULLUP);
+    pinMode(JS_1_X, INPUT);
+    pinMode(JS_1_Y, INPUT);
+
+    // JOYSTICK 2 control
+    pinMode(JS_2_BUTTON, INPUT_PULLUP); 
+    pinMode(JS_2_X, INPUT);
+    pinMode(JS_2_Y, INPUT);
+
+    // Initialize the Joystick library
+    Joystick.begin();
+    Joystick.setXAxisRange(-1, 1);
+    Joystick.setYAxisRange(-1, 1);
 }
 
-void loop() {
-    // Mouse Control:
-    int x_move = readAxis(A1);
-    int y_move = readAxis(A0);
-
-    Mouse.move(-x_move, y_move, 0);  
-
-    
-    if (digitalRead(JS_1_Button) != HIGH) {
-    // if the mouse is not pressed, press it:
-      if (!Mouse.isPressed(MOUSE_LEFT)) {
-        Mouse.press(MOUSE_LEFT);
-        delay(DELAY);
-      }
-    }
-    // else the mouse button is not pressed:
-    else {
-      // if the mouse is pressed, release it:
-      if (Mouse.isPressed(MOUSE_LEFT)) {
-        Mouse.release(MOUSE_LEFT);
-        delay(DELAY);
-      }
-    }
-    
-    
-    // WASD Control:
-    if(digitalRead(W_Button) == HIGH){
-      Keyboard.press('w');
-      delay(DELAY);
-    } else Keyboard.release('w');
-
-    if(digitalRead(S_Button) == HIGH){
-      Keyboard.press('s');
-      delay(DELAY);
-    } else Keyboard.release('s');
-
-    if(digitalRead(A_Button) == HIGH){
-      Keyboard.press('a');
-      delay(DELAY);
-    } else Keyboard.release('a');
-   
-   if(digitalRead(D_Button) == HIGH){
-      Keyboard.press('d');
-      delay(DELAY);
-   } else Keyboard.release('d');
-
-   //A1 / spacebar control
-   if(digitalRead(A1_Button) == HIGH){
-      Keyboard.write((char) 32);
-      delay(DELAY);
-    }
+void loop() { 
+  
+  delay(DELAY);
 }
 
